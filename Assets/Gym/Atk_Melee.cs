4867e56bdf1830041367665e46fddb280679835d
opachot <opachot@hotmail.com>
2016-10-06 19:50:02 -0400

    Basic melee trigger setup

---
+++ Assets/Gym/Atk_Melee.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Atk_Melee : MonoBehaviour {
+
+    #region DECLARATION
+    // CONST
+
+    // PRIVATE
+    private MeshCollider MeleeTrigger;
+
+    // PUBLIC
+
+    #endregion
+
+    #region UNITY METHODE
+    void Awake ()
+    {
+        MeleeTrigger = gameObject.transform.FindChild("Melee_Trigger").GetComponent<MeshCollider>();
+
+        MeleeTrigger.enabled = false;
+    }
+
+	void Start ()
+    {
+	    
+	}
+
+	void Update ()
+    {
+        
+	}
+    #endregion
+}
