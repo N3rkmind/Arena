ab308ae663b492d692eb74fd755c50ec0f2c37f7
opachot <opachot@hotmail.com>
2016-10-06 21:45:31 -0400

    Melee hit completed

---
+++ Assets/Script/Atk_Melee.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Atk_Melee : MonoBehaviour {
+
+    #region DECLARATION
+    // CONST
+    private const float DEFAULT_HIT_COOLDOWN = 1.0f;
+    private const float DEFAULT_HIT_DURATION = 0.5f;
+
+    // PRIVATE
+    private MeshCollider MeleeTrigger;
+
+    private float hitDurationTimer;
+    private float hitCooldownTimer;
+
+    private bool canHit;
+    private bool isHitting;
+
+    // PUBLIC
+
+    #endregion
+
+    #region UNITY METHODE
+    void Awake ()
+    {
+        MeleeTrigger = gameObject.transform.FindChild("Melee_Trigger").GetComponent<MeshCollider>();
+    }
+
+	void Start ()
+    {
+        MeleeTrigger.enabled = false;
+        canHit = true;
+	}
+
+	void Update ()
+    {
+        VerifyInput();
+
+        TryUpdateAllTimer();
+	}
+    #endregion
+
+
+    private void VerifyInput()
+    {
+        // Primary mouse button
+        if (Input.GetMouseButton(0))
+        {
+            if (canHit)
+            {
+                Hit();
+            }
+        }
+    }
+
+    private void Hit()
+    {
+        MeleeTrigger.enabled = true;
+        isHitting = true;
+        canHit = false;
+
+        hitCooldownTimer = DEFAULT_HIT_COOLDOWN;
+    }
+
+    #region TIMER METHODE
+    private void TryUpdateAllTimer()
+    {
+        if (!canHit)
+        {
+            if (isHitting)
+            {
+                UpdateHitDurationTimer();
+            }
+
+            UpdateHitCooldownTimer();
+        }
+    }
+
+
+    private void UpdateHitDurationTimer()
+    {
+        hitDurationTimer += Time.deltaTime;
+
+        if (hitDurationTimer >= DEFAULT_HIT_DURATION)
+        {
+            hitDurationTimer = 0;
+            MeleeTrigger.enabled = false;
+            isHitting = false;
+        }
+    }
+
+    private void UpdateHitCooldownTimer()
+    {
+        hitCooldownTimer -= Time.deltaTime;
+
+        if (hitCooldownTimer <= 0)
+        {
+            hitCooldownTimer = 0;
+            canHit = true;
+        }
+    }
+    #endregion
+}
---
+++ Assets/Script/PlayerTriggerListener.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTriggerListener : MonoBehaviour {
+
+    #region DECLARATION
+    // CONST
+    private const string MELEE_TRIGGER_NAME = "Melee_Trigger";
+
+    // PRIVATE
+    private MeshCollider ownMeleeTrigger;
+
+    // PUBLIC
+
+    #endregion
+
+    #region UNITY METHODE
+    void Awake()
+    {
+        ownMeleeTrigger = gameObject.transform.FindChild(MELEE_TRIGGER_NAME).GetComponent<MeshCollider>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.name == MELEE_TRIGGER_NAME)
+        {
+            if (col != ownMeleeTrigger)
+            {
+                //TODO: Call a methode on an player stats script insted
+                print("HIT!");
+            }
+        }
+    }
+    #endregion
+}
