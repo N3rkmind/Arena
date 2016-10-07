ceb448c8051d453cc9f9c0b5af715719483e69ec
Dmitriy Shulman <shulmandmitriy@gmail.com>
2016-10-07 21:28:14 +0300

    Add files via upload

---
+++ Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour 
+{
+	public GameObject options, leaderboard, achievement, mainmenu;
+
+	public void mainMenu()
+	{
+		mainmenu.SetActive (true);
+		options.SetActive (false);
+		leaderboard.SetActive (false);
+		achievement.SetActive (false);
+	}
+
+	public void Options()
+	{
+		mainmenu.SetActive (false);
+		options.SetActive (true);
+		leaderboard.SetActive (false);
+		achievement.SetActive (false);
+	}
+
+	public void Leaderboard()
+	{
+		mainmenu.SetActive (false);
+		options.SetActive (false);
+		leaderboard.SetActive (true);
+		achievement.SetActive (false);
+	}
+
+	public void Achievement()
+	{
+		mainmenu.SetActive (false);
+		options.SetActive (false);
+		leaderboard.SetActive (false);
+		achievement.SetActive (true);
+	}
+}
